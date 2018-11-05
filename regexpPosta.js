b6a0280b2e5094249f7c690d0ee983dba90d6aa0
zhilyaev <zhilyaev.dmitriy@gmail.com>
2018-11-06 01:56:26 +0300

    Posta Machine

---
+++ regexpPosta.js
@@ -0,0 +1,39 @@
+const main = (
+  L='111*1=',
+  A='1*=',
+  before=[
+    /111\*([1]+)=/,
+  ],
+  after=[
+    'xxx'
+  ]) => {
+  let contains = L.match(new RegExp(`^[${A}]+$`, 'g'))
+  if (!contains) {
+    console.error('Строка не состоит из Алфавита')
+    return L
+  }
+
+  let c = before.find(b => L.search(b) !==-1)
+
+  while (c) {
+    let j = before.indexOf(c)
+    let m = L.match(c)[1]
+    let r = L.replace(c, after[j])
+    L = r.replace(/x/g, m)
+    c = before.find(b => L.search(b) !==-1)
+  }
+
+  return L
+}
+
+
+// Read from files
+const fs = require('fs')
+console.log(
+  main(
+    fs.readFileSync('input/lenta', 'utf8'),
+    fs.readFileSync('input/alphabet', 'utf8'),
+    JSON.parse(fs.readFileSync('input/before.json', 'utf8')).map(v => new RegExp(v)),
+    JSON.parse(fs.readFileSync('input/after.json'))
+  )
+)
